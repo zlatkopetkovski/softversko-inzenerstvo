5c8cafa0789781dd6c459c090055431954e950e9
Zlatko <zlatko_petkovski@hotmail.com>
2016-12-13 19:27:38 +0100

    nice try - bed solution
    :/

---
+++ PosetiMe/PosetiMe/Controllers/tblCitiesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PosetiMe.Models;
+
+namespace PosetiMe.Controllers
+{
+    public class tblCitiesController : Controller
+    {
+        private DB_PosetiMeEntities db = new DB_PosetiMeEntities();
+
+        // GET: tblCities
+        public ActionResult Index()
+        {
+            return View(db.tblCities.ToList());
+        }
+
+        // GET: tblCities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCity tblCity = db.tblCities.Find(id);
+            if (tblCity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCity);
+        }
+
+        // GET: tblCities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblCities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] tblCity tblCity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblCities.Add(tblCity);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblCity);
+        }
+
+        // GET: tblCities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCity tblCity = db.tblCities.Find(id);
+            if (tblCity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCity);
+        }
+
+        // POST: tblCities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name")] tblCity tblCity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblCity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblCity);
+        }
+
+        // GET: tblCities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCity tblCity = db.tblCities.Find(id);
+            if (tblCity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCity);
+        }
+
+        // POST: tblCities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblCity tblCity = db.tblCities.Find(id);
+            db.tblCities.Remove(tblCity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PosetiMe/PosetiMe/Controllers/tblCommentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PosetiMe.Models;
+
+namespace PosetiMe.Controllers
+{
+    public class tblCommentsController : Controller
+    {
+        private DB_PosetiMeEntities db = new DB_PosetiMeEntities();
+
+        // GET: tblComments
+        public ActionResult Index()
+        {
+            var tblComments = db.tblComments.Include(t => t.tblLocal).Include(t => t.tblUser);
+            return View(tblComments.ToList());
+        }
+
+        // GET: tblComments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblComment tblComment = db.tblComments.Find(id);
+            if (tblComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblComment);
+        }
+
+        // GET: tblComments/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_Local = new SelectList(db.tblLocals, "ID", "Name");
+            ViewBag.ID_User = new SelectList(db.tblUsers, "ID", "UserName");
+            return View();
+        }
+
+        // POST: tblComments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ID_User,ID_Local,Comment")] tblComment tblComment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblComments.Add(tblComment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_Local = new SelectList(db.tblLocals, "ID", "Name", tblComment.ID_Local);
+            ViewBag.ID_User = new SelectList(db.tblUsers, "ID", "UserName", tblComment.ID_User);
+            return View(tblComment);
+        }
+
+        // GET: tblComments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblComment tblComment = db.tblComments.Find(id);
+            if (tblComment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_Local = new SelectList(db.tblLocals, "ID", "Name", tblComment.ID_Local);
+            ViewBag.ID_User = new SelectList(db.tblUsers, "ID", "UserName", tblComment.ID_User);
+            return View(tblComment);
+        }
+
+        // POST: tblComments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ID_User,ID_Local,Comment")] tblComment tblComment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblComment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_Local = new SelectList(db.tblLocals, "ID", "Name", tblComment.ID_Local);
+            ViewBag.ID_User = new SelectList(db.tblUsers, "ID", "UserName", tblComment.ID_User);
+            return View(tblComment);
+        }
+
+        // GET: tblComments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblComment tblComment = db.tblComments.Find(id);
+            if (tblComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblComment);
+        }
+
+        // POST: tblComments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblComment tblComment = db.tblComments.Find(id);
+            db.tblComments.Remove(tblComment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
