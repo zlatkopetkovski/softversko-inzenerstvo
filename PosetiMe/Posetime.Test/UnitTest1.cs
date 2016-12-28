df072377b826129f665022f96efb040a56ccfcd4
Zlatko <zlatko_petkovski@hotmail.com>
2016-12-28 10:10:37 +0100

    Add test

---
+++ PosetiMe/Posetime.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PosetiMe.Controllers;
+using NUnit.Framework;
+using NUnit;
+
+namespace Posetime
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CitiesIndex()
+        {
+            CitiesController city = new CitiesController();
+
+            var result = city.Details(1);
+            NUnit.Framework.Assert.AreEqual("Details", result);
+        }
+    }
+}
